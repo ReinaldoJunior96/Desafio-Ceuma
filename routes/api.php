cfb91c773728fdccfb6f3537f4839ba51800dc59
Reinaldo Júnior <33222779+ReinaldoJunior96@users.noreply.github.com>
2019-04-30 22:04:38 -0300

    Liberando API

--- routes/api.php
+++ routes/api.php
@@ -12,9 +12,12 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::post('/curso/alunos/{parameter}','CursoController@AlunosPorCurso');
-Route::get('/alunos/cursos','AlunoController@alunosjoincursos');
-Route::get('/alunos/contar','AlunoController@contar');
-//Route::post('/login/{user}/{senha}','UsuarioController@login');
-Route::resource('curso', 'CursoController');
-Route::resource('aluno', 'AlunoController');
+Route::group(['middleware' => 'cors'], function(){
+    Route::post('/curso/alunos/{parameter}','CursoController@AlunosPorCurso');
+	Route::get('/alunos/cursos','AlunoController@alunosjoincursos');
+	Route::get('/alunos/contar','AlunoController@contar');
+	//Route::post('/login/{user}/{senha}','UsuarioController@login');
+	Route::resource('curso', 'CursoController');
+	Route::resource('aluno', 'AlunoController');
+}); 
+
